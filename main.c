77e5eddc5a6a24b3da8d537079b901d919de17fd
DimasRidhwanaS <dimsridhwana@gmail.com>
2023-04-10 05:40:58 +0700

    Coba 3

--- main.c
+++ main.c
@@ -1,63 +1,61 @@
 #include <stdio.h>
-#define MAX_N 10
+#define MAX_SIZE 10
 
-int matrix[MAX_N][MAX_N];
-int visited[MAX_N][MAX_N];
-int cluster[MAX_N][MAX_N];
-int n;
+int visited[MAX_SIZE][MAX_SIZE] = {0};
 
-void createCluster(int row, int col, int clusterNo, int patokan, int toleransi) {
-    if(row < 0 || col < 0 || row >= n || col >= n) {
+void dfs(int mat[MAX_SIZE][MAX_SIZE], int i, int j, int patokan, int toleransi) {
+    if (i < 0 || j < 0 || i >= MAX_SIZE || j >= MAX_SIZE) {
         return;
     }
-    if(visited[row][col] || matrix[row][col] < patokan-toleransi || matrix[row][col] > patokan+toleransi) {
+    if (visited[i][j] || mat[i][j] < patokan - toleransi || mat[i][j] > patokan + toleransi) {
         return;
     }
-    visited[row][col] = 1;
-    cluster[row][col] = clusterNo;
-    createCluster(row+1, col, clusterNo, patokan, toleransi);
-    createCluster(row-1, col, clusterNo, patokan, toleransi);
-    createCluster(row, col+1, clusterNo, patokan, toleransi);
-    createCluster(row, col-1, clusterNo, patokan, toleransi);
+    visited[i][j] = 1;
+    dfs(mat, i+1, j, patokan, toleransi);
+    dfs(mat, i-1, j, patokan, toleransi);
+    dfs(mat, i, j+1, patokan, toleransi);
+    dfs(mat, i, j-1, patokan, toleransi);
 }
 
-int main() {
-    char fileName[50];
-    int patokan, toleransi, i, j, k, l, clusterCount = 0;
-
-    printf("Masukkan nama file: ");
-    scanf("%s", fileName);
-
-    FILE *file = fopen(fileName, "r");
-    if(file == NULL) {
-        printf("File tidak ditemukan.\n");
-        return 0;
-    }
-
-    fscanf(file, "%d", &n);
-    for(i=0; i<n; i++) {
-        for(j=0; j<n; j++) {
-            fscanf(file, "%d", &matrix[i][j]);
-        }
-    }
-    fclose(file);
-
-    printf("Masukkan nilai patokan: ");
-    scanf("%d", &patokan);
-
-    printf("Masukkan nilai toleransi: ");
-    scanf("%d", &toleransi);
-
-    for(i=0; i<n; i++) {
-        for(j=0; j<n; j++) {
-            if(!visited[i][j] && matrix[i][j] >= patokan-toleransi && matrix[i][j] <= patokan+toleransi) {
-                clusterCount++;
-                createCluster(i, j, clusterCount, patokan, toleransi);
+int count_clusters(int mat[MAX_SIZE][MAX_SIZE], int patokan, int toleransi) {
+    int i, j, count = 0;
+    for (i = 0; i < MAX_SIZE; i++) {
+        for (j = 0; j < MAX_SIZE; j++) {
+            if (!visited[i][j] && mat[i][j] >= patokan - toleransi && mat[i][j] <= patokan + toleransi) {
+                dfs(mat, i, j, patokan, toleransi);
+                count++;
             }
         }
     }
+    return count;
+}
 
-    printf("Jumlah klaster: %d\n", clusterCount);
+int main() {
+    char filename[20];
+    int patokan, toleransi;
+    printf("Masukkan nama file: ");
+    scanf("%s", filename);
+    printf("Masukkan nilai patokan: ");
+    scanf("%d", &patokan);
+    printf("Masukkan nilai toleransi: ");
+    scanf("%d", &toleransi);
 
+    FILE *fp;
+    fp = fopen(filename, "r");
+    if (fp == NULL) {
+        printf("File tidak ditemukan.\n");
+        return 0;
+    }
+    int mat[MAX_SIZE][MAX_SIZE];
+    int i, j;
+    for (i = 0; i < MAX_SIZE; i++) {
+        for (j = 0; j < MAX_SIZE; j++) {
+            fscanf(fp, "%d,", &mat[i][j]);
+        }
+    }
+    fclose(fp);
+
+    int jumlah_klaster = count_clusters(mat, patokan, toleransi);
+    printf("Jumlah klaster: %d\n", jumlah_klaster);
     return 0;
 }
